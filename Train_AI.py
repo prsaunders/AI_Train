533260e0d32d43ec206483caa34d8c289ffeb4e8
Paul R. Saunders <prsaunders@comcast.net>
2022-04-06 13:48:09 -0400

    fixed

--- Train_AI.py
+++ Train_AI.py
@@ -1,5 +1,5 @@
-from math import fabs
-from operator import lt
+# from math import fabs
+# from operator import lt
 import os
 import sys
 
@@ -17,7 +17,7 @@ from IOPi import IOPi
 # import threading
 from time import sleep, time, ctime  # , time
 from adafruit_servokit import ServoKit
-from psutil import cpu_times_percent
+# from psutil import cpu_times_percent
 
 #from Train_Hardware_Test_and_Setup import Save_Train_Inventory
 
